3c4d4bcc0758857c377b5d59eb4c6c659a67bc4c
dtvinh <duongvinh1706@gmail.com>
2022-10-05 13:58:31 +0700

    commit changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EMPTY, empty } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public name = 'VinhTester';
+  public age = 24;
+  // public fruits = ['Apple', 'Orange', 'Banana'];
+  public fruitAndPrice = [
+    { fruit: 'Apple', price: 50,flashSale:false,moreInfo:"Are in high price!" },
+    { fruit: 'Orange', price: 35,flashSale:true,moreInfo:"Are in low price!" },
+    { fruit: 'Banana', price: 38,flashSale:true,moreInfo:"Normally"}];
 
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void { //Console.log('') will print out everything to the console of the website
+    console.log('fruit is ', this.fruitAndPrice);
+    console.log('You are running Angular');
   }
-
 }
