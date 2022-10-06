315fd1b9c6a95d25d6a8dcfc5935bddc4b674c30
dtvinh <duongvinh1706@gmail.com>
2022-10-06 15:53:01 +0700

    commit code - routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [  { path: 'about', component: AboutComponent }, //localhost:4200/about will navigate to about component
+  { path: 'home', component: HomeComponent }, //localhost:4200/home will navigate to home component
+    { path: '', component: HomeComponent }, //If path not found, navigate to home
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
